2ba6bbe946b5777d4f944477b3fad4843b0455ef
Rezky Putra Barens <92147766+rezkybarensstudent@users.noreply.github.com>
2023-05-09 12:42:40 +0700

    Delete Boxplot.m

--- Boxplot.m
+++
@@ -1,7 +0,0 @@
-% Boxplot
-boxplot(DataSet81, 'notch', 'on')
-% To obtain the values of and explicitly.
-q1 = prctile(DataSet81, 25)
-q3 = prctile(DataSet81, 75)
-% To determine whether the data are symmetrically distributed about the mean
-s = skewness(DataSet81)
