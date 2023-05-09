c7517fedda6c14f4dfff2404807f26093cef5bea
Rezky Putra Barens <92147766+rezkybarensstudent@users.noreply.github.com>
2023-05-09 12:43:04 +0700

    Delete Histogram.m

--- Histogram.m
+++
@@ -1,5 +0,0 @@
-% If the bin centers had not been specified, then the resulting histogram would look
-% slightly different because the centers of the bins would be different.
-[nn, b] = hist(DataSet81, 9);
-bar(b, nn, 0.95, 'y');
-axis([70, 250, 0, max(nn)])
